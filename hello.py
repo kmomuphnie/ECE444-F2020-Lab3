052f6f584e160f745e451704b3cc07fff1e634d4
Dongfang <kmomuphnie@gmail.com>
2020-10-02 18:20:23 -0400

    Update hello.py
    Example 3-4. hello.py: Flask-Bootstrap initialization

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 # ...
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
 return render_template('index.html')
