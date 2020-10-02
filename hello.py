808a5a9342f3d5531d5a8a8cc80b6bbbdf96e377
Dongfang <kmomuphnie@gmail.com>
2020-10-02 18:48:21 -0400

    Update hello.py
    Example 3-11. hello.py: Initialize Flask-Moment

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-# ...
+from flask_moment import Moment
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
@@ -17,7 +19,7 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
-	
+
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
