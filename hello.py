f43274bbe0271229e087bed4ccc62d7977101550
Dongfang <kmomuphnie@gmail.com>
2020-10-02 18:27:04 -0400

    Update hello.py
    added main into hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,11 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-return render_template('index.html')
+	return render_template('index.html')
+
 @app.route('/user/<name>')
 def user(name):
-return render_template('user.html', name=name)
+	return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
