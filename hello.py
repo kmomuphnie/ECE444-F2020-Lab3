4fc57386496ce4ae962e4d75a9a205ca8a5ecffd
Dongfang <kmomuphnie@gmail.com>
2020-10-02 20:48:04 -0400

    Update hello.py
    Acticity 2 is done. Added email block.

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required
+from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -17,12 +18,20 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+        	flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+
+
         return redirect(url_for('index'))
-    return render_template('index.html',form = form, name = session.get('name'))
+    return render_template('index.html',form = form, name = session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
@@ -39,6 +48,7 @@ def internal_server_error(e):
 
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
+	email = EmailField('What is your UofT Email address?')
 	submit = SubmitField('Submit')
 
 if __name__ == '__main__':
